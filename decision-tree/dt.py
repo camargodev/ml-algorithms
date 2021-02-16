b9c14074d8293e0cf8fdbef280e3964272f2911b
camargodev <camargodev@gmail.com>
2021-02-15 22:10:28 -0300

    Reading TSV + holdout

--- decision-tree/dt.py
+++ decision-tree/dt.py
@@ -1,22 +1,35 @@
 import graphviz 
-from sklearn.datasets import load_iris
+import csv
+
 from sklearn import tree
+from sklearn.model_selection import train_test_split
+
+from pandas import DataFrame
+from pandas import read_csv
 
 ENTROPY = "entropy"
 GINI = "gini"
+TEST_SIZE = 0.2
+SEED = 1405
 
 def read_training_data():
-    return load_iris(return_X_y=True)
+    return read_csv('data/vote.tsv', sep='\t', header=0)
 
-def train(criterion, parameters, classes):
+def holdout(training_data):
+    training_data_frame =  DataFrame(training_data)
+    features = training_data_frame.drop("target", axis=1)
+    target = training_data_frame["target"]
+    return train_test_split(features, target, test_size=TEST_SIZE, random_state=SEED)
+
+def train(criterion, features, target):
     decision_tree = tree.DecisionTreeClassifier(criterion=criterion)
-    return decision_tree.fit(parameters, classes)
+    return decision_tree.fit(features, target)
 
-def train_with_entropy(parameters, classes):
-    return train(ENTROPY, parameters, classes)
+def train_with_entropy(features, target):
+    return train(ENTROPY, features, target)
 
-def train_with_gini(parameters, classes):
-    return train(GINI, parameters, classes)
+def train_with_gini(features, target):
+    return train(GINI, features, target)
     
 def export_output(name, decision_tree):
     dot_data = tree.export_graphviz(decision_tree, out_file=None, 
@@ -25,7 +38,18 @@ def export_output(name, decision_tree):
     graph = graphviz.Source(dot_data)  
     graph.render(name) 
 
+training_data = read_training_data()
+train_features, test_features, train_target, test_target = holdout(training_data)
 
-parameters, classes = read_training_data()
-decision_tree = train_with_entropy(parameters, classes)
-export_output("test", decision_tree)
+print("\ntrain_features")
+print(train_features)
+print("\ntest_features")
+print(test_features)
+print("\ntrain_target")
+print(train_target)
+print("\ntest_target")
+print(test_target)
+
+# parameters, classes = read_training_data()
+# decision_tree = train_with_entropy(parameters, classes)
+# export_output("test", decision_tree)
